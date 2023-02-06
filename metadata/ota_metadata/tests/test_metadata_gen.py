ac318bf31b028484d251b169b0e5a483deca92ae
hiroyuki.obinata <hiroyuki.obinata@tier4.jp>
2023-02-06 15:06:47 +0900

    add .flake8

--- metadata/ota_metadata/tests/test_metadata_gen.py
+++ metadata/ota_metadata/tests/test_metadata_gen.py
@@ -12,9 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import pytest
-import shutil
-import os
 from pathlib import Path
 from pytest_unordered import unordered
 
